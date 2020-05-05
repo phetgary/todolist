cd9b2573efde63e1d20178c98e20a0fecb346be1
phetgary <56030176+phetgary@users.noreply.github.com>
2020-05-05 21:05:47 +0800

    add click n remove

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
    $(this).toggleClass("completed");    
+});
+
+$("ul").on("click", "span", function(event){
+   $(this).parent().fadeOut(500,function(){
+      $(this).remove();
+   });
+   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(){
+      if(event.which === 13){
+         var todoText = $(this).val();
+         $(this).val("");
+         $("ul").append("<li><span>X</span> "+ todoText + " </li>")
+      }
 });
